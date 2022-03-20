6b156ac00660435b3449288d556b54d15fc6d6af
Sbbars <sbbarse2121@gmail.com>
2022-03-20 22:29:02 +0530

    CodeChef Solutions

---
+++ Guess the bottom face.py
@@ -0,0 +1,6 @@
+T = int(input())
+for i in range(T):
+    X = int(input())
+    sumFace = 7
+    ans = abs(X-sumFace)
+    print(ans)
---
+++ How much Scholarship.py
@@ -0,0 +1,7 @@
+X = int(input())
+if X<=50:
+    print(100)
+elif 50 < X <= 100:
+    print(50)
+else:
+    print(0)
